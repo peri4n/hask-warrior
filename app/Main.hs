c92cd0ed17da40cecd619598b09a2d142fdcefe8
Fabian Bull <fabian@fbull.de>
2021-11-22 19:17:16 +0100

    Add logging MTL style

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,54 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main where
 
-import Options.Applicative
+import ClassyPrelude
+import Control.Monad.IO.Class
+import Control.Monad.Logger
 import Lib
+import Options.Applicative
 
 type TaskId = Int
 
 data Command
-  = Insert TaskId 
+  = Add TaskId
   | Delete TaskId
 
 main :: IO ()
-main = run =<< execParser
+main = app =<< execParser
     (parseCommand `withInfo` "Interactive Task Manager")
 
 parseCommand :: Parser Command
 parseCommand = subparser $
-  command "insert" (parseInsert `withInfo` "Create a task") <>
-  command "delete" (parseInsert `withInfo` "Delete a task")
+  command "add" (parseAdd `withInfo` "Create a task") <>
+  command "delete" (parseDelete `withInfo` "Delete a task")
 
-parseInsert :: Parser Command
-parseInsert = Insert <$> argument auto (metavar "TASKID")
+parseAdd :: Parser Command
+parseAdd = Add <$> argument auto (metavar "TASKID")
 
 parseDelete :: Parser Command
 parseDelete = Delete <$> argument auto (metavar "TASKID")
 
-run :: Command -> IO ()
-run (Insert id) = putStrLn $ "Inserting Task " ++ (show id)
-run (Delete id) = putStrLn $ "Deleting Task " ++ (show id)
+app :: Command -> IO ()
+app (Add id) = runStdoutLoggingT (runHask $ addTask id)
+app (Delete id) = runStdoutLoggingT (runHask $ deleteTask id)
+
+addTask :: TaskId -> Hask ()
+addTask id = do
+  logInfoN $ "Adding task with id " <> (tshow id)
+  liftIO $ putStrLn "Write to DB"
+
+deleteTask :: TaskId -> Hask ()
+deleteTask id = do
+  logInfoN $ "Deleting task with id " <> (tshow id)
+  liftIO $ putStrLn "Remove from DB"
 
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc
+
+newtype Hask a = Hask { runHask :: LoggingT IO a }
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadIO
+           , MonadLogger)
